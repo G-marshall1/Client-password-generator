bc529fbda2afaa3a972e539673f15367b890b448
G Marshall <gmarshalltech1@gmail.com>
2023-03-11 19:35:49 -0700

    Added all possible charaters that can be added.

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,27 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// What does this code do?
 
+
+// Assignment Code
+// I need to Define all possible characters that can be used in the password.
+// I need to define the length of the password.
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const specialCharacters = "!@#$%^&*()_+~`|}{[];?><,./-=";
+
+
+// we are targeting the button with the id of generate
+var generateBtn = document.querySelector("#generate") 
+
+
+function generatePassword() {
+  // This is confirming that the generatePassword function is running in the console
+console.log("generatePassword function is running")
+}
 // Write password to the #password input
 function writePassword() {
+  // Generate password is not defined
+  // the generatePassword function should store the password when called (currently no generatePassword is being defined because no value is given)
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -11,4 +30,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+
+// by clicking the button, the function writePassword should run
 generateBtn.addEventListener("click", writePassword);
