272d44b520e0bfa87ede147d7f6373a83b06bd6f
G Marshall <gmarshalltech1@gmail.com>
2023-03-14 09:27:09 -0600

    my generator was only producing as long of a random gen as it needed but it kept using the same 4 letters. I was able to fix this by moving my confirms under my function writePassword.

--- Develop/script.js
+++ Develop/script.js
@@ -10,8 +10,6 @@ var numbers = "0123456789";
 var specialCharacters = "!@#$%^&*()_+~`|}{[];?><,./-=";
 var password = "";
 
-// I need to define the length of the password.
-var passwordLength = prompt("How many characters would you like your password to be?");
 
 // we are targeting the button with the id of generate
 var generateBtn = document.querySelector("#generate") 
@@ -20,11 +18,6 @@ var generateBtn = document.querySelector("#generate")
 var passwordText = document.querySelector("#password");
 
 // added confirm prompts to ask the user if they want to use lower case, upper case, numbers, and special characters
-var lowerCase = confirm("Would you like to use lower case letters?");
-var upperCase = confirm("Would you like to use upper case letters?"); 
-var numbers = confirm("Would you like to use numbers?");
-var specialCharacters = confirm("Would you like to use special characters?");
-var password = "";
 
 while (!lowerCase && !upperCase && !numbers && !specialCharacters) {
   alert("You must choose at least one character type!");
@@ -35,34 +28,43 @@ while (!lowerCase && !upperCase && !numbers && !specialCharacters) {
 }
 
 function generatePassword() {
-char = "";
-if (lowerCase) {
-  char += lowerCase;
+  char = "";
+  if (lowerCase) {
+    char += lowerCase;
+  }
   if (upperCase) {
     char += upperCase;
-    if (numbers) {
-      char += numbers;
-      if (specialCharacters) {
-        char += specialCharacters;
-      }
-    }
   }
- password = "";
-for (var i = 0; i < passwordLength; i++) {
-  password += char.charAt(Math.floor(Math.random() * char.length));
+  if (numbers) {
+    char += numbers;
+  }
+  if (specialCharacters) {
+    char += specialCharacters;
+  }
+  
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    password += char.charAt(Math.floor(Math.random() * char.length));
+  }
+  return password;
 }
-return password;
-}
-}
+
 // Write password to the #password input
+// I need to define the length of the password.
+var passwordLength = prompt("How many characters would you like your password to be?");
 function writePassword() {
+  var lowerCase = confirm("Would you like to use lower case letters?");
+  var upperCase = confirm("Would you like to use upper case letters?"); 
+  var numbers = confirm("Would you like to use numbers?");
+  var specialCharacters = confirm("Would you like to use special characters?");
+  var password = "";
   // Generate password is not defined
   // the generatePassword function should store the password when called (currently no generatePassword is being defined because no value is given)
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
